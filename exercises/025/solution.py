3272879257fe7ea3e0462397ec4420ad05f544bd
prospermax89 <mpmax@hotmail.fr>
2015-09-21 17:22:07 +0200

    j'ai ajouté une ligne à la fin

---
+++ exercises/025/solution.py
@@ -0,0 +1,9 @@
+import datetime
+
+a = datetime.datetime.now().year
+b = datetime.datetime.now().month
+c = datetime.datetime.now().day
+d = datetime.datetime.now().hour
+e = datetime.datetime.now().minute
+f = datetime.datetime.now().second
+print('Today is',a,'-',b,'-',c,'and it is',d,':',e,':',f)
