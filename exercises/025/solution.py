ad46d762a9622c9230a49a625afe1f62e0c571ff
prospermax89 <mpmax@hotmail.fr>
2015-09-21 17:30:16 +0200

    j'ai ajouté une ligne à la fin

--- exercises/025/solution.py
+++ exercises/025/solution.py
@@ -1,11 +1,8 @@
 import datetime
-
 a = datetime.datetime.now().year
 b = datetime.datetime.now().month
 c = datetime.datetime.now().day
 d = datetime.datetime.now().hour
 e = datetime.datetime.now().minute
 f = datetime.datetime.now().second
-print(' Today is ',a,'-',b,'-',c,' and it is ',d,':',e,':',f, sep='')
-
-
+print(' Today is ', a, '-', b, '-', c, ' and it is ', d,':', e,':', f, sep='')
