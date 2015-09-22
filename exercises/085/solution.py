32b8b77f82f1ff7cd1c7ac6b75382b1749671eeb
prospermax89 <mpmax@hotmail.fr>
2015-09-22 13:04:02 +0200

    j'ai ajouté une ligne à la fin

--- exercises/085/solution.py
+++ exercises/085/solution.py
@@ -13,3 +13,17 @@ def sort_by_name(e):
     import operator
     f = sorted(e, key=operator.itemgetter(1))
     print(f)
+
+
+g = [[85, 'Susan Maddox'],
+ [84, 'Joseph Pedersen'],
+ [42, 'Lidia Robel'],
+ [37, 'Jeanette Wafer'],
+ [36, 'John Freeman'],
+ [27, 'Betty Askins'],
+ [22, 'Mark Osbourne'],
+ [12, 'Bonnie Torres'],
+ [6, 'Joshua Tran']]
+ 
+sort_by_name(g)
+ 
