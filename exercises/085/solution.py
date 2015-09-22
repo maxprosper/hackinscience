0dcbede25fc0701e9e72a7e17c1e84f07dc326ca
prospermax89 <mpmax@hotmail.fr>
2015-09-22 12:56:49 +0200

    j'ai ajouté une ligne à la fin

--- exercises/085/solution.py
+++ exercises/085/solution.py
@@ -1,10 +1,14 @@
 def sort_a_list(a):
     b = sorted(a, reverse=True)
     print(b)
+
+
 def sort_by_mark(c):
     import operator
     d = sorted(c, key=operator.itemgetter(0), reverse=True)
     print(d)
+
+
 def sort_by_name(e):
     import operator
     f = sorted(e, key=operator.itemgetter(1))
