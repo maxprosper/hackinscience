9a6981f90a7d1e7256b7a9fe7f8e6f15ee099f31
prospermax89 <mpmax@hotmail.fr>
2015-09-22 11:21:55 +0200

    j'ai ajouté une ligne à la fin

--- exercises/080/solution.py
+++ exercises/080/solution.py
@@ -3,5 +3,7 @@ let = "abcdefghijklmnopqrstuvwxyz"
 liste = []
 for i in range(0, 26 * 26):
     if liste.count(let[int((i / 26))] + let[operator.mod(i, 26)]) == 0:
-        liste.append(let[int((i / 26))] + let[operator.mod(i, 26)])
-        print(let[int((i / 26))] + let[operator.mod(i, 26)])
+        if liste.count(let[operator.mod(i, 26)] + let[int((i / 26))]) == 0:
+            if let[int((i / 26))] != let[operator.mod(i, 26)]:
+                liste.append(let[int((i / 26))] + let[operator.mod(i, 26)])
+                print(let[int((i / 26))] + let[operator.mod(i, 26)])
