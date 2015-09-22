3b7dfdf2cfa295c58cd7fdb9de67a4393fa60653
prospermax89 <mpmax@hotmail.fr>
2015-09-22 15:33:55 +0200

    j'ai ajouté une ligne à la fin

---
+++ exercises/100/solution.py
@@ -0,0 +1,12 @@
+station = {
+ 'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
+ 'number': 31705,
+ 'latitude': 48.8645278209514,
+ 'name': 'CHAMPEAUX (BAGNOLET)',
+ 'longitude': 2.416170724425901
+}
+print("latitude" + ' ' + str(station["latitude"]))
+print("longitude" + ' ' + str(station["longitude"]))
+print("number" + ' ' + str(station["number"]))
+print("name" + ' ' + str(station["name"]))
+print("address" + ' ' + str(station["address"]))
