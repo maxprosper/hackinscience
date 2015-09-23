147f0ea1d5a36fa6d56bb6688039f021450ba3c6
prospermax89 <mpmax@hotmail.fr>
2015-09-23 18:12:27 +0200

    j'ai ajouté une ligne à la fin

--- exercises/200/solution.py
+++ exercises/200/solution.py
@@ -3,12 +3,7 @@ def is_prime(a):
         return True
     else:
         for i in range(2, int(a ** 0.5) + 2):
-            print(i)
             if a % i == 0:
                 return False
             else:
                 return True
-
-print(is_prime(1))
-print(is_prime(2))
-print(is_prime(3))
