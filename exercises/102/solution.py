5910a8dd6b78dcb63ef0da977da11c4d082827a6
prospermax89 <mpmax@hotmail.fr>
2015-09-23 10:04:01 +0200

    j'ai ajouté une ligne à la fin

--- exercises/102/solution.py
+++ exercises/102/solution.py
@@ -45,4 +45,5 @@ def check_my_city(z):
             list = {"station_nb": a, "zip_code": b, "city": f}
             return(list)
         if a == 0:
-            return('"Sorry! No station for your city has been found!"')
+            return("Sorry! No station for your city has been found!")
+print(check_my_city("LILLE"))
