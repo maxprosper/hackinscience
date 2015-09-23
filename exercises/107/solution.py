54acbaeabe81cfcbe5186699038801c88f78e27f
prospermax89 <mpmax@hotmail.fr>
2015-09-23 12:27:41 +0200

    j'ai ajouté une ligne à la fin

--- exercises/107/solution.py
+++ exercises/107/solution.py
@@ -9,5 +9,9 @@ def select_student(z, g):
             refused.append(z[i])
     toto = sorted(accepted, key=lambda w: w[1], reverse=True)
     tutu = sorted(refused, key=lambda w: w[1])
-    dico = {'Accepted': str(toto), 'Refused': str(tutu)}
+    dico = {'Accepted': toto, 'Refused': tutu}
     return(print(dico))
+
+my_class = [['Kermit Wade', 27], ['Hattie Schleusner', 67], ['Ben Ball', 5], ['William Lee', 2]]
+
+select_student(my_class, 20)
