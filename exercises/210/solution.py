6139ae8159282d221534505eb8dd34ae4d10fdfa
prospermax89 <mpmax@hotmail.fr>
2015-09-24 11:02:42 +0200

    j'ai ajouté une ligne à la fin

---
+++ exercises/210/solution.py
@@ -0,0 +1,19 @@
+def is_prime(a):
+    z = 0
+    if a == 1:
+        return False
+    if a == 2:
+        return True
+    else:
+        for i in range(2, int(a ** 0.5) + 2):
+            if a % i == 0:
+                z = 1
+        if z == 1:
+            return False
+        else:
+            return True
+l = 0
+for y in range(0, 1000):
+    if is_prime(y) == True:
+        l = l + y
+print(l)
