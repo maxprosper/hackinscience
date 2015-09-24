dc80342343e13c4ea811be70bce821c390003042
prospermax89 <mpmax@hotmail.fr>
2015-09-24 09:29:57 +0200

    j'ai ajouté une ligne à la fin

--- exercises/201/solution.py
+++ exercises/201/solution.py
@@ -1,5 +1,6 @@
 def is_letter(a):
-    if a in ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]:
+    if a in ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]:
         return(True)
     else:
         return(False)
