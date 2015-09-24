d47bb21f85277814936c670e4945a6afa0ce42c8
prospermax89 <mpmax@hotmail.fr>
2015-09-24 10:21:16 +0200

    j'ai ajouté une ligne à la fin

--- exercises/202/solution.py
+++ exercises/202/solution.py
@@ -2,8 +2,8 @@ def starts_with(haystack, needle):
     if len(haystack) != len(needle):
         return False
     for i in range(0, len(needle) + 1):
-        z = 0            
-        if haystack[i] != needle[i] :
+        z = 0
+        if haystack[i] != needle[i]:
             z = 1
         if z == 1:
             return False
