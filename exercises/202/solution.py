e0c8b0078ff833c6fbd1b2e0c7e8d05e661af0c9
prospermax89 <mpmax@hotmail.fr>
2015-09-24 10:00:41 +0200

    j'ai ajouté une ligne à la fin

--- exercises/202/solution.py
+++ exercises/202/solution.py
@@ -1,7 +1,7 @@
 def starts_with(a, b):
     for i in range(0, len(b)):
         z = 0
-        if a[i] != b[i]:
+        if a[i] != b[i] or len(a[i]) != len(b[i]):
             z = 1
         if z == 1:
             return False
